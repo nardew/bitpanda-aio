ea6638153451b2da131a2535342f846a7e7deb9a
Melvin Strobl <melvin.strobl@web.de>
2020-07-12 08:54:58 +0200

    changed timer issue so that it uses milliseconds instead of ns

--- bitpanda/Timer.py
+++ bitpanda/Timer.py
@@ -8,9 +8,11 @@ class Timer(object):
 		self.name = name
 		self.active = active
 
+		self.current_milli_time = lambda: int(round(time.time() * 1000))
+
 	def __enter__(self):
-		self.start_tmstmp = time.time_ns()
+		self.start_tmstmp = self.current_milli_time()
 
 	def __exit__(self, type, value, traceback):
 		if self.active:
-			LOG.debug(f'Timer {self.name} finished. Took {round((time.time_ns() - self.start_tmstmp) / 1000000, 3)} ms.')
+			LOG.debug(f'Timer {self.name} finished. Took {(self.current_milli_time() - self.start_tmstmp)} ms.')
